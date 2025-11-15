312892232c37ddb0dd23a5e94fc7f6f5fed0e5b7
wswalker87 <scott@wscottwalker.com>
2025-11-15 10:06:57 -0600

    init commit

--- User.py
+++ User.py
@@ -1,1 +1,16 @@
 # your User class goes here
+class User:
+
+    def __init__(self, first_name, last_name, email_address, license_number):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email_address = email_address
+        self.license_number = license_number
+
+    def user_input(self):
+        first_name = input("Enter your first name: ")
+        print(f'First name is confirmed as: {first_name}.')
+        last_name = input("Enter your last name: ")
+        print(f'Last name is confirmed as: {last_name}.')
+        email_address = input("Enter your email address: ")
+        print(f'Confirmed')
